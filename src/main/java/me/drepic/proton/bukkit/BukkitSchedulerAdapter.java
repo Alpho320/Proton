88313d04586919a17e39ac47e56ab398244df514
Alpho320 <cahitbaba00@gmail.com>
2025-08-26 19:05:16 +0300

    add folia support

--- src/main/java/me/drepic/proton/bukkit/BukkitSchedulerAdapter.java
+++ src/main/java/me/drepic/proton/bukkit/BukkitSchedulerAdapter.java
@@ -1,23 +1,27 @@
 package me.drepic.proton.bukkit;
 
+import com.github.Anon8281.universalScheduler.UniversalScheduler;
+import com.github.Anon8281.universalScheduler.scheduling.schedulers.TaskScheduler;
 import me.drepic.proton.common.adapters.SchedulerAdapter;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class BukkitSchedulerAdapter implements SchedulerAdapter {
 
     private JavaPlugin plugin;
+    private TaskScheduler taskScheduler;
 
     protected BukkitSchedulerAdapter(JavaPlugin plugin) {
         this.plugin = plugin;
+        this.taskScheduler = UniversalScheduler.getScheduler(plugin);
     }
 
     @Override
     public void runTask(Runnable runnable) {
-        this.plugin.getServer().getScheduler().runTask(this.plugin, runnable);
+        this.taskScheduler.runTask(runnable);
     }
 
     @Override
     public void runTaskAsynchronously(Runnable runnable) {
-        this.plugin.getServer().getScheduler().runTaskAsynchronously(this.plugin, runnable);
+        this.taskScheduler.runTaskAsynchronously(runnable);
     }
 }
